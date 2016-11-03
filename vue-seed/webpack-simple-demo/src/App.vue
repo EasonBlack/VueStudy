9e878534be153b346638648ea8f360fc1e92abbc
easonblack <eason820720@163.com>
2016-11-03 14:51:28 +0800

    v

---
+++ vue-seed/webpack-simple-demo/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="app">
+		{{message}}
+	</div>
+</template>
+
+<script>
+	export default {
+	    name: 'app',
+		data() {
+			return {
+				message: 'my vue'
+			}
+		}
+	}
+</script>
+<style>
+
+</style>
