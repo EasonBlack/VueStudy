f2365f15bcc30a1dae0308348992e098df14868f
EasonBlack <eason820720@163.com>
2016-11-22 07:56:35 +0800

    d

--- vue-animate/3d-loop-by-index/app-item.js
+++ vue-animate/3d-loop-by-index/app-item.js
@@ -2,15 +2,38 @@ Vue.component('app-item', {
     props: ['item', 'index'],
     template: `<li class="app__item"
              
-                   :style="{ bottom: 200 * ( 0.45 * index) + 'px' , 
-                         right: index * 2 + 'px',
-                         width: 300 * (1 - 0.15 * index) + 'px' ,
-                         height: 300 * (1 - 0.2 * index)+ 'px',
+                   :style="{ bottom: _bottom , 
+                         right: _right,
+                         width: _width ,
+                         height: _height,
                          'background-color': item.bgc,
-                         opacity: 1 - index * 0.2,
+                         opacity: _opacity,
                          zIndex: 20-index 
                    }"
                 >
          This is Item {{item.title}}  {{index}}
-     </li>`
+     </li>`,
+    computed: {
+        _bottom: function () {
+            return 200 * (0.45 * this.index ) + 'px'
+        },
+        _right: function () {
+            return this.index * 2 + 'px'
+        },
+        _width: function () {
+            return 300 * (1 - 0.15 * this.index) + 'px'
+        },
+        _height: function () {
+            return 300 * (1 - 0.2 * this.index) + 'px'
+        },
+        _opacity: function () {
+            if (this.item.ready) {
+                return 0.3
+            } else {
+                return 1 - this.index * 0.2
+            }
+
+        }
+
+    }
 })
--- vue-animate/3d-loop-by-index/app.js
+++ vue-animate/3d-loop-by-index/app.js
@@ -1,27 +1,16 @@
 Vue.component('app', {
     props: [],
     template: `<div class="app__container">
-         <ul class="app__list"> 
-           <!--<li v-for="(itemback,index) in backItems" class="app__item"-->
-               <!--v-bind:key="itemback"-->
-               <!--v-bind:class="{blur: itemback.blur}"-->
-               <!--:style="{ bottom: 200 * ( 0.45 * index) + 'px' , -->
-                         <!--right: index * 2 + 'px',-->
-                         <!--width: 300 * (1 - 0.15 * index) + 'px' ,-->
-                         <!--height: 300 * (1 - 0.2 * index)+ 'px',-->
-                         <!--'background-color': itemback.bgc,-->
-                         <!--opacity: 1 - index * 0.2,-->
-                         <!--zIndex: 20-index -->
-                   <!--}"-->
-           <!--&gt;-->
-                <!--This is Item {{itemback.title}} -->
-           <!--</li>-->
-           <app-item  v-for="(item, index) in items" 
+ 
+         <transition-group name="list" tag="ul" class="app__list">
+          <app-item  v-for="(item, index) in items" 
                     :item="item" 
                     :index="index" 
+                    :key = 'item'
                     >
            </app-item>
-         </ul>
+         </transition-group>
+          
          <ul>
             <li v-for="item in originItems">
                <input type="radio" name="cc" v-on:click="selectIt(item)" />
@@ -33,35 +22,35 @@ Vue.component('app', {
      </div>`,
     data: ()=> {
         return {
-            originItems: [
-            ],
+            originItems: [],
             items: [
-                {id: 1, title: 'One', bgc: 'steelblue'},
-                {id: 2, title: 'Two', bgc: 'lightseagreen'},
-                {id: 3, title: 'Three', bgc: 'lightcoral'},
-                {id: 4, title: 'Four', bgc: 'lightyellow'}
+                {id: 1, title: 'One', bgc: 'steelblue', ready: false},
+                {id: 2, title: 'Two', bgc: 'lightseagreen', ready: false},
+                {id: 3, title: 'Three', bgc: 'lightcoral', ready: false},
+                {id: 4, title: 'Four', bgc: 'lightyellow', ready: false}
             ],
             firstItem: {}
         }
     },
     mounted: function () {
-         this.originItems = this.items.slice();
+        this.originItems = this.items.slice();
     },
     methods: {
         selectIt: function (item) {
             let _index = this.items.findIndex((t) => t.id == item.id);
-            // this.backItems = [];
-            // for (let i = 0; i < _index; i++) {
-            //     this.backItems.push(this.items[i]);
-            //     setTimeout(() => {
-            //         this.backItems[this.backItems.length - 1].blur = true;
-            //     }, 10);
-            // }
             for (let i = 0; i < _index; i++) {
-                let _theItem = this.items[0];
-                this.items.splice(0, 1);
-                this.items.push(_theItem);
+                this.items[i].ready = true;
             }
+            setTimeout(()=> {
+                for (let i = 0; i < _index; i++) {
+                    let _theItem = this.items[0];
+                    _theItem.ready = false;
+                    this.items.splice(0, 1);
+                    setTimeout(()=> {
+                        this.items.push(_theItem);
+                    }, 300)
+                }
+            }, 300)
         },
 
     }
