7c2bcb399b66438e6b3b3d5450aa7fe2fc12a652
easonblack <eason820720@163.com>
2016-11-24 17:50:38 +0800

    ss

---
+++ vue-component/slot-multi/demo-components.js
@@ -0,0 +1,21 @@
+Vue.component('view1', {
+    props: [],
+    template: `<div id="view1">
+        This is View1
+    </div>`
+});
+
+Vue.component('view2', {
+    props: [],
+    template: `<div id="view2">
+        This is View2
+    </div>`
+});
+
+Vue.component('view3', {
+    props: [],
+    template: `<div id="view3">
+        This is View3
+    </div>`
+});
+
