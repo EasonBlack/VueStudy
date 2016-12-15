bd2ac8578907e077f10f1903858ca89468994552
easonblack <eason820720@163.com>
2016-12-15 10:27:28 +0800

    sdf

---
+++ vue-chart/basic/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div id="app">
+		 <Chart></Chart>
+	</div>
+</template>
+
+<script>
+	import Chart from './Chart.vue'
+	export default {
+	    components: {Chart},
+		data() {
+			return {}
+		}
+	}
+</script>
+<style >
+ #app {
+    width:50vw;
+    height:50vh;
+ }
+</style>
---
+++ vue-chart/basic/src/Chart.vue
@@ -0,0 +1,60 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    import Highcharts from 'highcharts';
+    export default{
+        data(){
+            return{
+                 target: undefined
+            }
+        },
+        mounted : function() {
+            this.target = Highcharts.chart(this.$el, {
+                  chart: {
+                      type: 'bar'
+                  },
+                  title: {
+                      text: 'Stacked bar chart'
+                  },
+                  xAxis: {
+                      categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+                  },
+                  yAxis: {
+                      min: 0,
+                      title: {
+                          text: 'Total fruit consumption'
+                      }
+                  },
+                  legend: {
+                      reversed: true
+                  },
+                  plotOptions: {
+                      series: {
+                          stacking: 'normal'
+                      }
+                  },
+                  series: [{
+                      name: 'John',
+                      data: [5, 3, 4, 7, 2]
+                  }, {
+                      name: 'Jane',
+                      data: [2, 2, 3, 2, 1]
+                  }, {
+                      name: 'Joe',
+                      data: [3, 4, 4, 2, 5]
+                  }]
+            });
+        },
+        beforeDestroy: function() {
+            this.target.destroy();
+        }
+    }
+
+
+</script>
+
+<style scoped>
+
+</style>
