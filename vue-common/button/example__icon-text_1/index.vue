20679f2f207330b49b0433bb1813e8a778a5e598
easonblack <eason820720@163.com>
2016-12-16 14:31:29 +0800

    button

---
+++ vue-common/button/example__icon-text_1/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <a class='button__wrapper' v-on:click='clickHandle'>
+        <img :src="item.src ? item.src : null"/>
+        <span>{{item.text}}</span>
+    </a>
+</template>
+
+<script>
+
+    export default{
+        props: ['item'],
+        data:function(){
+            return {
+
+            }
+        },
+        methods: {
+            clickHandle:function() {
+                this.$emit('clickHandle');
+            }
+        },
+
+    }
+</script>
+
+<style lang='scss' scoped>
+    .button__wrapper {
+        display: inline-flex;
+        align-items: center;
+        height:40px;
+        line-height:40px;
+        padding: 0px 20px;
+        background-color: white;
+        color:#AFAFAF;
+        img {
+            width:0px;
+            height:0px;
+        }
+        img[src] {
+            width:30px;
+            height:30px;
+        }
+    }
+
+</style>
---
+++ vue-common/button/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+	<div id="app">
+		<div class='button__container'>
+            <example-button :item='item1' v-on:clickHandle='image1Handle'></example-button>
+        </div>
+        <div class='button__container'>
+            <example-button :item='item2' v-on:clickHandle='image1Handle'></example-button>
+        </div>
+
+	</div>
+</template>
+
+<script>
+    import exampleButton from '../example__icon-text_1/index.vue';
+    import img1 from 'assets/icons/order.png'
+    import img2 from 'assets/icons/Forward.png'
+	export default {
+	    name: 'app',
+	    components: {exampleButton},
+		data() {
+			return {
+                item1 : {
+                    src:  img1,
+                    text: "Add a Dataset"
+                },
+                item2 : {
+                    src:  false,
+                    text: "Back"
+                }
+			}
+		},
+		methods: {
+		    image1Handle: function() {
+
+		    }
+		},
+		created:function() {
+
+		}
+
+	}
+</script>
+<style>
+    * {
+        padding:0;
+        margin:0;
+        box-sizing:border-box;
+        font-family: Arial;
+    }
+
+    #app {
+        min-height:100vh;
+        background-color:#EFEFEF;
+        padding-top:50px;
+        padding-left:50px;
+    }
+
+    .button__container {
+        margin-bottom:10px;
+    }
+
+
+</style>
