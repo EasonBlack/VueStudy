3c571a46a5035541a3a2e7e139104667c22061e4
EasonBlack <eason820720@163.com>
2016-12-18 11:25:31 +0800

    ds

--- vue-chart/practise/dashboard/src/view-board/index.vue
+++ vue-chart/practise/dashboard/src/view-board/index.vue
@@ -1,21 +1,30 @@
 <template>
     <div class='container'>
-        <app-button :item='addButton'></app-button>
-        This is board
+        <div  class='top__wrapper'>
+             <a class='button__wrapper' v-on:click='toggleAddPanel(1)'>
+                 <img src="~assets/icons/plus_white.png"/>
+                 <span>Add a Dataset</span>
+             </a>
+        </div>
+        <div class='content__wrapper'>
+            This is board
+        </div>
+        <new-panel :active='newPanelActive' v-on:toggleAddPanel='toggleAddPanel' ></new-panel>
     </div>
 </template>
 
 <script>
-    import appButton from 'common/button/example__icon-text_1/index.vue';
-    import plusIcon from 'assets/icons/plus_white.png';
+    import newPanel from './new-panel.vue';
     export default{
-        components: {appButton},
+        components: {newPanel},
         data(){
             return{
-               addButton : {
-                   src:  plusIcon,
-                   text: "Add a Dataset"
-               }
+                newPanelActive:false
+            }
+        },
+        methods: {
+            toggleAddPanel:function(v) {
+                this.newPanelActive = !!v;
             }
         }
 
@@ -29,5 +38,37 @@
         height:calc(100vh - #{$headerHeight});
         background-color: $dark-bgc;
         color:white;
+        position:relative;
+        overflow:hidden;
     }
+
+    .top__wrapper {
+        width:100vw;
+        height: $headerHeight;
+        padding: $left-padding;
+    }
+
+    .button__wrapper {
+          display: inline-flex;
+          align-items: center;
+          height:40px;
+          line-height:40px;
+          padding: 0px 20px;
+          background-color: $bgc;
+          color:white;
+
+          &:hover {
+            background-color: $light-bgc;
+          }
+
+          img[src] {
+              width:10px;
+              height:10px;
+              margin-right: 10px;
+          }
+     }
+
+     .content__wrapper {
+        padding:$left-padding;
+     }
 </style>
---
+++ vue-chart/practise/dashboard/src/view-board/new-panel.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class='container'
+        v-bind:class='{
+            active: active
+        }'
+        v-on:click='toggleAddPanel'
+        >
+
+    </div>
+</template>
+
+<script>
+    export default{
+        props: ['active'],
+        data(){
+            return{
+
+            }
+        },
+        methods: {
+            toggleAddPanel:function() {
+                this.$emit('toggleAddPanel',0);
+            }
+        }
+    }
+</script>
+
+<style lang='scss' scoped>
+    .container {
+        width:100vw;
+        height:300px;
+        background-color: steelblue;
+        position:absolute;
+        bottom:0;
+        left:0;
+        margin-bottom:-300px;
+        transition:all 0.5s ease-out;
+        &.active {
+            margin-bottom:0px;
+        }
+    }
+
+
+</style>
