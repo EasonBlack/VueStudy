189ad30a9eca04db2cd4baea50b8b4fd9bf49389
EasonBlack <eason820720@163.com>
2017-01-07 11:42:05 +0800

    calendar

--- vue-common/calendar/example__calendar__1/index.vue
+++ vue-common/calendar/example__calendar__1/index.vue
@@ -1,45 +1,56 @@
 <template>
     <div class='calendar__container'>
        <div class='date__container' v-for='i in countDays'>
-            {{getDate(i)}}
+            <date-div
+            :dateMoment='getDateMoment(i)'
+            :dateSource = 'getDateSource(i)'
+            :currentMonth='currentMonth'></date-div>
        </div>
     </div>
 </template>
 
 <script>
     import moment from 'moment';
+    import dateDiv from './date.vue';
     export default{
-        props: ['currentYear','currentMonth'],
+        components: {dateDiv},
+        props: ['currentYear','currentMonth', 'source'],
         data:function(){
             return {
-
+                startDate: '',
+                endDate: '',
+                countDays: ''
             }
-        },
-        created:function() {
-            console.log(moment().format('YYYY-MM-DD'));
-            console.log(moment().format('d'));
         },
         methods: {
-            getDate: function(i) {
-                return moment(this.startDate).add(i-1,'days').format('YYYY-MM-DD');
+            getDateMoment: function(i) {
+                return moment(this.startDate).add(i-1,'days');
+            },
+            getDateSource: function(i) {
+                let _d = moment(this.startDate).add(i-1,'days').format('YYYY-MM-DD');
+                return this.source.find((o)=> o.date == _d) || {}
             }
         },
+
         computed: {
-            'startDate': function(){
-                let _first = moment({y: this.currentYear, m: this.currentMonth, d:1}).startOf('week').format('YYYY-MM-DD');
-                return _first;
-            },
-            'endDate': function(){
-                let monthEnd = moment({y: this.currentYear, m: this.currentMonth, d:1}).endOf('month').format('YYYY-MM-DD');
-                let _end = moment(monthEnd).endOf('week').format('YYYY-MM-DD');
-                return _end;
-            },
-            'countDays': function() {
-                let _end = moment(this.endDate);
-                let _start = moment(this.startDate);
-                return _end.diff(_start, 'days') + 1;
-            }
-        }
+             'startDate': function(){
+                 let _year = this.currentYear;
+                 let _month = this.currentMonth;
+                 return  moment({y: _year, month: _month, d:1}).startOf('week').format('YYYY-MM-DD');
+             },
+             'endDate': function(){
+                 let _year = this.currentYear;
+                 let _month = this.currentMonth;
+                 let monthEnd = moment({y: _year, month: _month, d:1}).endOf('month').format('YYYY-MM-DD');
+                 let _end = moment(monthEnd).endOf('week').format('YYYY-MM-DD');
+                 return _end;
+             },
+             'countDays': function() {
+                 let _end = moment(this.endDate);
+                 let _start = moment(this.startDate);
+                 return _end.diff(_start, 'days') + 1;
+             }
+         }
     }
 </script>
 
--- vue-common/calendar/src/App.vue
+++ vue-common/calendar/src/App.vue
@@ -1,7 +1,15 @@
 <template>
 	<div id="app">
 		<div class='main_container'>
-            <example-calendar :currentYear='currentYear' :currentMonth='currentMonth'></example-calendar>
+		    <div class='calendar__nav'>
+		        <a class='nav__prev' v-on:click='prev'>Prev</a>
+		        <div class='nav__title'>{{this.current.format('YYYY-MM')}}</div>
+		        <a  class='nav__next'  v-on:click='next'>Next</a>
+		    </div>
+            <example-calendar
+            :currentYear='currentYear'
+            :currentMonth='currentMonth'
+            :source='source'></example-calendar>
         </div>
 
 
@@ -9,6 +17,7 @@
 </template>
 
 <script>
+    import Vue from 'vue';
     import moment from 'moment';
     import exampleCalendar from '../example__calendar__1/index.vue';
 	export default {
@@ -16,13 +25,34 @@
 	    components: {exampleCalendar},
 		data() {
 			return {
-                currentMoment: moment().month(),
-                currentYear: moment().year()
+			    current: moment().startOf('month'),
+			    currentText: moment().startOf('month').format('YYYY-MM-DD'),
+                currentMonth: moment().month(),
+                currentYear: moment().year(),
+                source: []
             }
-		}
+		},
+		created: function(){
+		    Vue.http.get('../data/source.json').then((response) => {
+                this.source = response.body;
+            })
+		},
+        methods: {
+            prev: function() {
+                this.current =this.current.subtract(1, 'months');
+
+                this.currentMonth = this.current.month();
+                this.currentYear = this.current.year();
+            },
+            next: function() {
+                this.current = this.current.add(1, 'months');
+                this.currentMonth = this.current.month();
+                this.currentYear = this.current.year();
+            }
+        }
 	}
 </script>
-<style>
+<style lang='scss' scoped>
     * {
         padding:0;
         margin:0;
@@ -38,6 +68,21 @@
 
     .main_container {
 
+    }
+
+    .calendar__nav {
+        display:flex;
+        width:200px;
+        .nav__prev,.nav__next {
+            flex:1;
+            display: block;
+        }
+        .nav__title {
+            flex: 1;
+        }
+        .nav__next {
+            text-align: right;
+        }
     }
 
 </style>
