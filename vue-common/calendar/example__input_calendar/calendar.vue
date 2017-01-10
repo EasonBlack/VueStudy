5279b37efd304bbfb7460185e0ad3d4966ec46f0
easonblack <eason820720@163.com>
2017-01-10 17:47:58 +0800

    ss

--- vue-common/calendar/example__input_calendar/calendar.vue
+++ vue-common/calendar/example__input_calendar/calendar.vue
@@ -1,15 +1,72 @@
 <template>
     <div class='calendar__container'>
-
+        <div class='calendar__title-section'>
+            <span class='calendar__prev'>
+                <img src='../../../assets/icons/Back-50.png' v-on:click='prev' />
+            </span>
+            <span class='calendar__title'>{{currentYear + '-' + currentMonth}}</span>
+            <span class='calendar__next'>
+                <img src='../../../assets/icons/Forward-50.png' v-on:click='next' />
+            </span>
+        </div>
+        <div class='calendar__content-section'>
+            <date-div
+                    v-for='i in countDays'
+                    :dateMoment='getDateMoment(i)'
+                    :currentMonth='currentMonth'>
+            </date-div>
+        </div>
     </div>
 </template>
 
 <script>
+    import moment from 'moment';
+    import dateDiv from './date.vue';
     export default{
+        components: {dateDiv},
         data(){
             return{
-                msg:'hello vue'
+                current: moment(),
+                currentYear: moment().year(),
+                currentMonth: moment().format('MM')
             }
+        },
+        methods: {
+            getDateMoment: function(i) {
+                console.log(moment(this.startDate).add(i-1,'days'));
+                return moment(this.startDate).add(i-1,'days');
+            },
+            prev:function() {
+                this.current = this.current.subtract(1,'month');
+                this.currentYear = this.current.year();
+                this.currentMonth = this.current.format('MM');
+            },
+            next:function() {
+                this.current = this.current.add(1,'month');
+                 this.currentYear = this.current.year();
+                this.currentMonth = this.current.format('MM');
+            }
+        },
+        computed: {
+            'startDate': function() {
+                 let _year = this.currentYear;
+                 let _month = this.current.month();
+                 console.log(moment({y: _year, month: _month, d:1}).startOf('week').format('YYYY-MM-DD'))
+                 return  moment({y: _year, month: _month, d:1}).startOf('week').format('YYYY-MM-DD');
+            },
+             'endDate': function(){
+                 let _year = this.currentYear;
+                 let _month =  this.current.month();
+                 let monthEnd = moment({y: _year, month: _month, d:1}).endOf('month').format('YYYY-MM-DD');
+                 let _end = moment(monthEnd).endOf('week').format('YYYY-MM-DD');
+                 return _end;
+             },
+             'countDays': function() {
+                 let _end = moment(this.endDate);
+                 let _start = moment(this.startDate);
+                 console.log(_end.diff(_start, 'days') + 1)
+                 return _end.diff(_start, 'days') + 1;
+             }
         }
     }
 </script>
@@ -20,7 +77,45 @@
         top:30px;
         left:0;
         width:100%;
-        height:300px;
+        padding-bottom: 20px;
         background-color:steelblue;
+        z-index:10;
+
     }
+
+    .calendar__title-section {
+        height:30px;
+        line-height:30px;
+        display:flex;
+        .calendar__prev {
+            display:flex;
+            flex:1;
+            img {
+                width:25px;
+            }
+        }
+        .calendar__next {
+            display:flex;
+            flex:1;
+            justify-content: flex-end;
+            img {
+                width:25px;
+            }
+        }
+        .calendar__title {
+            display:flex;
+            flex:2;
+            color:white;
+            justify-content: center;
+        }
+    }
+    .calendar__content-section {
+        width:100%;
+        display:flex;
+        position:relative;
+        flex-wrap:wrap;
+        z-index:10;
+    }
+
+
 </style>
---
+++ vue-common/calendar/example__input_calendar/date.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class='date__container'
+         v-bind:class="{
+                notcurrent: !isCurrentMonth
+             }">
+        {{date}}
+    </div>
+</template>
+
+<script>
+    import moment from 'moment';
+    export default {
+        props: ['dateMoment', 'currentMonth'],
+        data: function(){
+            return {}
+        },
+        computed: {
+            'date': function() {
+                if(!this.dateMoment) {
+                    return '';
+                }
+                return this.dateMoment.date();
+            },
+            'isCurrentMonth':function() {
+                if(!this.dateMoment) {
+                    return false;
+                }
+                console.log(this.dateMoment.format('MM'), this.currentMonth)
+                return this.dateMoment.format('MM') == this.currentMonth;
+            }
+        }
+    }
+</script>
+
+<style lang='scss' scoped>
+    .date__container {
+        flex: 1 0 14%;
+        display:flex;
+        flex-direction:column;
+        border-bottom: 1px solid rgba(grey, 0.5);
+        border-right: 1px solid rgba(grey, 0.5);
+        height: 30px;
+        position:relative;
+        background-color:white;
+        color:black;
+        justify-content: center;
+        align-items: center;
+        &:hover {
+            background-color: rgba(steelblue, 0.3);
+            color:white;
+        }
+        &.notcurrent {
+            color:grey;
+        }
+    }
+</style>
