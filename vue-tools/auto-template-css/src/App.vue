c4c1b89c31bb83f053d02421e14c178ce858cc23
easonblack <eason820720@163.com>
2017-02-28 16:37:37 +0800

    auto html-css

---
+++ vue-tools/auto-template-css/src/App.vue
@@ -0,0 +1,158 @@
+<template>
+	<div class='main__section'>
+		<div class='input__section'>
+            <textarea v-model='inputOrigin' @keydown='keydownHandle'></textarea>
+        </div>
+		<div class='display__section'>
+            <div class='html__section'>
+                <html-display :inputTags='inputTags' :tabnum='tabnum'></html-display>
+            </div>
+            <div class='css__section'>
+                <css-display :inputTags='inputTags'></css-display>
+            </div>
+
+        </div>
+	</div>
+</template>
+
+<script>
+    import htmlDisplay from './html-display/index.vue';
+    import cssDisplay from './css-display/index.vue';
+	export default {
+        components: {htmlDisplay, cssDisplay},
+	    data() {
+	    	return {
+                inputOrigin: '',
+                tabnum: {
+                    0: '',
+                    1: '\t',
+                    2: '\t\t',
+                    3: '\t\t\t',
+                    4: '\t\t\t\t',
+                    5: '\t\t\t\t\t',
+                }
+	    	}
+	    },
+	    created: function() {
+             this.checkTab = function(s, n) {
+                let num = 0;
+                for(let i=n;i>0;i--) {
+                    if(s.indexOf(this.tabnum[i]) != -1) {
+                        num = i;
+                        break;
+                    }
+                }
+                return num;
+            },
+            this.recodeTags = function(tags) {
+                let prev = null;
+                for(let i =0;i<tags.length;i++) {
+                    let t = tags[i];
+                    if(prev && t.num > prev.num) {
+                        t.parent = prev.index;
+                    } else if(prev && t.num == prev.num) {
+                        t.parent = prev.parent;
+                    } else if(prev && t.num < prev.num) {
+                        let pid = prev.parent;
+                        let p = tags.find(o=> {
+                            return o.index == pid
+                        })
+                        console.log(p);
+                        t.parent = p.parent!=undefined ? p.parent : p.index;
+                    }
+                    prev = t;
+                }
+                return tags;
+            }
+	    },
+	    methods:  {
+	        keydownHandle: function(e) {
+	            let el = e.target;
+                if(e.keyCode === 9)  {
+                     var start = el.selectionStart;
+                     var end = el.selectionEnd;
+                     var value = el.value;
+
+                     this.inputOrigin = this.inputOrigin.substring(0, start)
+                                 + "\t"
+                                 + this.inputOrigin.substring(end);
+
+                     el.selectionStart = el.selectionEnd = start + 1;
+                     e.preventDefault();
+                }
+	        }
+	    },
+	    computed: {
+            inputTags: function() {
+                let initNum = 1;
+                let lines = this.inputOrigin.split('\n');
+                let seperators =[];
+                while(this.inputOrigin.indexOf(this.tabnum[initNum])!=-1) {
+                    initNum++;
+                }
+
+                let tags = lines.map((o, i)=> {
+                    let num = this.checkTab(o, initNum);
+                    let arr = o.trim().split('@');
+                    let self = arr[0];
+                    let selfArr = self.split('_')
+                    let children = arr.slice(1,arr.length);
+                    children = children.map((o)=> {
+                        let _tempArr =  o.split('_');
+                        return {
+                            el: _tempArr[0],
+                            css: _tempArr.slice(1,_tempArr.length)
+                        }
+                    });
+                    return {
+                        text: selfArr[0],
+                        css: selfArr.slice(1,selfArr.length),
+                        children: children,
+                        num : num,
+                        index: i
+                    }
+                })
+                tags = this.recodeTags(tags);
+                return tags;
+            }
+	    }
+	}
+</script>
+<style lang='scss' scoped >
+    .main__section {
+        display:flex;
+        widht:100vw;
+        height:100vh;
+        overflow:hidden;
+        position:relative;
+    }
+    .input__section {
+        width:50%;
+        height:100%;
+
+        border-right:1px solid rgba(grey,0.3);
+        textarea {
+            width:100%;
+            height:100%;
+            padding:10px;
+            resize:none;
+            &:focus {
+                outline:0;
+            }
+        }
+    }
+    .display__section {
+        width:50%;
+        height:100%;
+    }
+    .html__section {
+        height:50%;
+        padding:10px;
+        border-bottom:1px solid rgba(grey, 0.3);
+    }
+    .css__section {
+        padding:10px;
+        height:50%;
+    }
+
+</style>
---
+++ vue-tools/auto-template-css/src/html-display/index.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class='display__wrapper'>{{displayTxt}}
+    </div>
+</template>
+
+<script>
+
+    export default{
+        props: ['inputTags', 'tabnum'],
+        data(){
+            return{
+
+            }
+        },
+        created: function() {
+            this.buildTreeTag = (tag, tags) => {
+                let _html = `${this.tabnum[tag.num || 0]}<div class='${tag.text}__container'>\n\r`;
+                let id = tag.index;
+                let children = tags.filter((o)=> {
+                    return o.parent == id
+                })
+                tag.items =[...children];
+                for(let i =0;i<children.length;i++) {
+                    let currentTag = children[i];
+                    let currentTagHtml = this.buildTreeTag(currentTag, tags);
+                    _html += currentTagHtml;
+                }
+                for(let c of tag.children){
+                    _html += this.tabnum[tag.num +1 ] + `<${c.el} />\n\r`
+                }
+                _html+= this.tabnum[tag.num] + '</div>\n\r';
+                return _html;
+            }
+        },
+        computed: {
+            displayTxt: function() {
+                let html = '';
+                html = this.buildTreeTag(this.inputTags[0], this.inputTags);
+                return html
+            }
+        }
+
+    }
+
+</script>
+
+<style lang='scss' scoped>
+    .display__wrapper {
+
+        white-space:pre-wrap;
+        overflow:auto;
+    }
+</style>
