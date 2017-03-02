4d1d43ee60a4a2701a445eea9ce1aedded937047
EasonBlack <eason820720@163.com>
2017-03-03 00:28:29 +0800

    fetch

---
+++ vue-demo/resourse_multi_fetch/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+	<div id="app">
+		Hello {{name1}}, {{name2}}
+	</div>
+</template>
+
+<script>
+	export default {
+	    name: 'app',
+		data() {
+			return {
+				message: 'my vue',
+				name1: '',
+				name2: ''
+
+			}
+		},
+		created: function() {
+            Promise.all([
+                this.$http.get('../data/a.json'),
+                this.$http.get('../data/b.json')
+            ]).then((data)=> {
+               this.name1=data[0].body.name;
+               this.name2=data[1].body.name;
+            });
+		}
+	}
+</script>
+<style>
+
+</style>
