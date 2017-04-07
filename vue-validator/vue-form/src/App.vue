a8d9a64a4fef166ea08440ee86303061fdd4eb6b
easonblack <eason820720@163.com>
2017-04-07 17:54:07 +0800

    uuu

--- vue-validator/vue-form/src/App.vue
+++ vue-validator/vue-form/src/App.vue
@@ -5,11 +5,34 @@
                 <validate >
                         <label>Name</label>
                         <input type="text" name="name" required v-model="model.name">
-                        <field-messages name="name" show="$touched" >
-                          <div>Success!</div>
-                          <div slot="required">Name is a required field</div>
-                        </field-messages>
                 </validate>
+                <field-messages name="name" show="$touched || $submitted" >
+                        <div  class="success__msg">Success!</div>
+                        <div class="error__msg" slot="required">Name is a required field</div>
+                </field-messages>
+
+
+               <validate :custom="{testv2: testv2}">
+                   <label>NAME2</label>
+                   <input  type="text" name="name2" required :testv2="model.name" v-model="model.name2"/>
+               </validate>
+               <field-messages name="name2" show="$touched || $submitted" >
+                   <div class="success__msg">Success!</div>
+                   <div class="error__msg" slot="testv2">not = name</div>
+                   <div class="error__msg" slot="required">this is a required field</div>
+               </field-messages>
+
+               <validate :custom="{testv: testv}">
+                   <label>EEE</label>
+                   <input  type="text" name="eee" required v-model="model.eee"/>
+               </validate>
+               <field-messages name="eee" show="$touched || $submitted" >
+                   <div class="success__msg">Success!</div>
+                   <div class="error__msg" slot="testv">not = abc</div>
+                   <div class="error__msg" slot="required">this is a required field</div>
+               </field-messages>
+
+
            </vue-form>
            <div>{{model}}</div>
         </div>
@@ -26,10 +49,20 @@
 			    formstate : {},
                 model: {
                     name: '',
-                    name2: ''
+                    name2: '',
+                    eee: ''
                 }
 			}
-		}
+		},
+        methods: {
+            testv: function(value) {
+                return value == 'abc'
+            },
+            testv2: function(value,attr) {
+                console.log(value, attr);
+                return value == attr
+            }
+        }
 
 	}
 </script>
