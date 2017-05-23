3f0105a224c5a4f4b5cd6fa5479dfc569ff1ae9c
EasonBlack <eason820720@163.com>
2017-05-23 23:10:02 +0800

    dff

---
+++ vue-begin/vue-extend/demo/src/base.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+       This is base view {{message}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            }
+        },
+        created: function() {
+            console.log('created!!');
+        }
+    }
+
+</script>
+
+<style lang="scss" scoped>
+    .container {
+        color: red;
+    }
+</style>
---
+++ vue-begin/vue-provider-inject/demo/src/grandchild.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>This is grand child, {{message}}</div>
+</template>
+
+<script>
+    export default {
+        inject: ['message']
+    }
+</script>
