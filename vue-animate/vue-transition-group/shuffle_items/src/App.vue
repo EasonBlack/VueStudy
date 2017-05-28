202115376c97588329fd2d1f1b6f4335f84fbf51
EasonBlack <eason820720@163.com>
2017-05-28 16:12:04 +0800

    rtrt

---
+++ vue-animate/vue-transition-group/shuffle_items/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class='list__container'>
+		<transition-group name="item-list" tag="div">
+            <item-div v-for='(item, index) in sortedItems' :key='item'
+                :item="item" :index="index"
+                @update='update'
+            ></item-div>
+        </transition-group>
+	</div>
+</template>
+
+<script>
+    import itemDiv from './item/index.vue';
+	export default {
+	    components: {itemDiv},
+		data() {
+			return {
+			    items: [
+			        {title: 'aa', num: 6},
+			        {title: 'bb', num: 5},
+			        {title: 'cc', num: 4},
+			        {title: 'dd', num: 3},
+			        {title: 'ee', num: 2},
+			        {title: 'ff', num: 1}
+			    ]
+			}
+		},
+		methods: {
+		    update(num, index) {
+		        this.items[index].num = num;
+		    }
+		},
+		computed: {
+		    sortedItems() {
+		        return this.items.sort((a,b)=> {
+		            return a.num - b.num
+		        })
+		    }
+		}
+	}
+</script>
+<style lang='scss' scoped>
+    .list__container {
+    }
+    .item-list-move {
+        transition: transform 1s;
+    }
+</style>
---
+++ vue-animate/vue-transition-group/shuffle_items/src/item/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div
+        class='item__container'
+    >
+        <div class='item_bar'
+            :style="{
+                width: item.num * 5 +'px'
+             }">
+             {{item.title}}
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['item', 'index'],
+        data() {
+            return {
+            }
+        },
+        created() {
+            setInterval(()=>{
+                let _num = Math.floor(Math.random() * 10);
+                this.$emit('update', parseInt(this.item.num) + _num, this.index);
+            },2000)
+        },
+        computed: {
+
+        }
+    }
+</script>
+<style lang='scss' scoped>
+    .item__container {
+        position:relative;
+    }
+    .item_bar {
+        color:white;
+        background-color: steelblue;
+        margin-bottom:5px;
+        transition:  width 1s;
+    }
+</style>
