95863e6023ff9b9339159303a7cc671510b2b75d
EasonBlack <eason820720@163.com>
2017-06-01 22:46:56 +0800

    yuyu

---
+++ vue-begin/vue-extend/demo-base/src/App.vue
@@ -0,0 +1,25 @@
+
+
+<script>
+    import base from './base.vue';
+    import myComponent from './real-component.vue';
+	export default {
+        extends: base,
+        components: {myComponent},
+		data() {
+			return {
+			    flagA: true,
+				message: 'my vue',
+				clickMethods: 'getB'
+			}
+		},
+		created: function() {
+		    console.log('created!!');
+		}
+	}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ vue-begin/vue-extend/demo-base/src/base.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+       This is base view {{message}}
+       <div v-if='flagA'>This is flag A</div>
+       <div v-if='flagB'>This is flag B</div>
+       <div v-if='flagC'>This is flag C</div>
+       <button @click='clickHandle'>Click Me</button>
+       <my-component></my-component>
+    </div>
+</template>
+
+<script>
+    import myComponent from './blank-component.vue';
+    export default {
+        components: {
+            myComponent
+        },
+        data() {
+            return {
+                flagA:false,
+                flagB: false,
+                flagC: false,
+                message: 'xxx',
+                clickMethods: "getA"
+            }
+        },
+        created: function() {
+            console.log('created!!');
+        },
+        methods: {
+            getA() {
+                alert('a');
+            },
+            getB() {
+                alert('b');
+            },
+            clickHandle() {
+                this[this.clickMethods]();
+            }
+        }
+    }
+
+</script>
+
+<style lang="scss" scoped>
+    .container {
+        color: red;
+    }
+</style>
