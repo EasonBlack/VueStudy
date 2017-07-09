96ae62f2b789eb403093f6466c63d7af8745e6d5
EasonBlack <eason820720@163.com>
2017-07-09 14:57:08 +0800

    eslint

---
+++ vue-webpack/webpack-vue-eslint/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class='main__container'>
+	     <div>Hello World</div>
+	     <div class='box'>
+	         This is a demo for autoprefixer
+	     </div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+				message: 'hello'
+			}
+		},
+		methods: {
+		a() {
+				this.message = 'xxxx'
+			}
+		}
+	}
+</script>
+<style lang='scss' scoped>
+    .main__container {
+        display: flex
+    }
+
+    .box {
+        width: 100px;
+        height: 100px;
+        background-color: #ccc;
+        color: white;
+        -webkit-border-radius: 5px;
+        border-radius: 5px;
+    }
+
+</style>
