4b8559022a1f11fdae11f99e7b914a95be2f7ca8
EasonBlack <eason820720@163.com>
2017-07-22 14:35:31 +0800

    try pug

---
+++ vue-pug/basic/src/App.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+	div {{message}}
+	    include test.html
+	    span    xxx
+</template>
+
+<script>
+	export default {
+	    name: 'app',
+		data() {
+			return {
+				message: 'my vue',
+				message2: 'test include'
+			}
+		}
+	}
+</script>
+<style>
+
+</style>
