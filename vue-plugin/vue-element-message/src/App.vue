1715a428494a24abc461a5006b9243e0e5998381
easonblack <eason820720@163.com>
2017-08-10 10:26:55 +0800

    updates

---
+++ vue-plugin/vue-element-message/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+       <button @click="confirm">Confirm</button>
+	</div>
+</template>
+
+<script>
+    import { MessageBox } from 'element-ui'
+	export default {
+		data() {
+			return {}
+		},
+        methods: {
+            confirm() {
+                MessageBox.alert('hello', 'xxx', {
+                    confirmButtonText: 'OK',
+                    callback: action => {
+                        console.log('yes');
+                    }
+                });
+            }
+        }
+	}
+</script>
+<style>
+    * {
+        padding:0;
+        margin:0;
+        box-sizing:border-box;
+        font-family: Arial;
+    }
+
+
+</style>
