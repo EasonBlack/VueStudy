1bfe01c43576ab9f8c680ec581f4ccd310e9c67e
EasonBlack <eason820720@163.com>
2017-08-20 02:12:39 +0800

    up

---
+++ vue-nav/header-nav-popup/demo_1/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="header__container">
+
+	        <div class='right__account' @click='toggleHeaderDrop'>
+	            Administrator Bradon
+                <header-drop v-if='showHeaderDrop' />
+	        </div>
+
+	</div>
+</template>
+
+<script>
+    import headerDrop from './headerDrop.vue';
+	export default {
+        components: {headerDrop},
+		data() {
+			return {
+			    showHeaderDrop: false
+			}
+		},
+		methods: {
+            toggleHeaderDrop() {
+                this.showHeaderDrop = !this.showHeaderDrop;
+            }
+		}
+	}
+</script>
+<style lang='scss' scoped>
+    .header__container {
+        height:50px;
+        line-height:50px;
+        display:flex;
+        justify-content: flex-end;
+        background-color: steelblue;
+    }
+
+    .right__account {
+        width:250px;
+        height:100%;
+        display:flex;
+        padding-left:20px;
+        align-items: center;
+        border-left: 1px solid white;
+        color: white;
+    }
+
+</style>
---
+++ vue-nav/header-nav-popup/demo_1/src/headerDrop.vue
@@ -0,0 +1,88 @@
+<template>
+    <ul class="header__drop">
+         <li class='header__drop_item'>
+            <div class='placeholder__icon_circle'></div>
+            <div>Account Setting</div>
+         </li>
+         <li class='header__drop_item header__drop_border'>
+             <div class='placeholder__icon_circle'></div>
+             <div>Help Center</div>
+         </li>
+         <li  class='header__drop_item'>
+             <div class='placeholder__icon_circle'></div>
+             <div>American</div>
+         </li>
+         <li  class='header__drop_item'>
+             <div class='placeholder__icon_circle'></div>
+             <div>Logout</div>
+         </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        created() {
+        },
+        methods: {
+        },
+        computed: {
+        }
+    }
+</script>
+
+<style lang='scss' scoped>
+     .header__drop {
+         position: absolute;
+         width:85%;
+         top: 100%;
+         left: 20px;
+         z-index:10;
+         background-color:white;
+     }
+     .header__drop:after {
+         position: absolute;
+         top:-10px;
+         right: calc(50% - 10px);
+         width: 0;
+         height: 0;
+         display: block;
+         content: '';
+         border-left: 10px solid transparent;
+         border-right: 10px solid transparent;
+         border-bottom: 10px solid white;
+     }
+
+     .header__drop {
+         margin-top: 10px;
+         max-width: 345px;
+         min-width:100px;
+     }
+     .header__drop_item {
+         cursor: pointer;
+         color: steelblue;
+         display: flex;
+         align-items: center;
+         padding-left:10px;
+         padding-right:10px;
+         a {
+            height:40px;
+            line-height:40px;
+            padding:10px;
+         }
+     }
+     .header__drop_border {
+        border-bottom: 1px solid grey;
+        margin-bottom:10px;
+        padding-bottom:10px;
+     }
+     .placeholder__icon_circle {
+        width:30px;
+        height:30px;
+        background-color: #eee;
+        border-radius: 50%;
+        margin-right:10px;
+     }
+</style>
