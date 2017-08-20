d94b2d52f26ce42a8bf7d00916200deb7097833e
EasonBlack <eason820720@163.com>
2017-08-20 17:30:16 +0800

    up

---
+++ vue-practise/form_page/demo01/src/App.vue
@@ -0,0 +1,149 @@
+<template>
+	<div class="login__container">
+
+		<div class='login__box'>
+
+		    <div class='login__login' >
+		        <div class='row'>
+                    <form-row-input :title='"Email"' :w='"35%"'>
+                        <input></input>
+                    </form-row-input>
+		        </div>
+		        <div class='row'>
+		            <form-row-input :title='"Password"' :w='"35%"' >
+                        <input></input>
+                    </form-row-input>
+		        </div>
+		        <div class='row'>
+		             <form-row-input :title='"Re Password"' :w='"35%"'>
+                        <input></input>
+                    </form-row-input>
+		        </div>
+                <div class='row'>
+		             <form-row-input :title='"City"' :title2='"State"' :w='"35%"'>
+                        <input></input>
+                        <input slot='input2'></input>
+                    </form-row-input>
+		        </div>
+
+		        <div class='row'>
+		            <form-row-input :title='"Code"' :w='"35%"' class='form__code'>
+                            <input ></input>
+                            <a>Get Code</a>
+                    </form-row-input>
+		        </div>
+		        <div class='row'>
+		            <div class='ml-35'>
+		                <input type=checkbox />
+		                <span>aaaaaa aaaaaa aaaaaa</span>
+		            </div>
+                </div>
+		        <div class='row'>
+		            <a class='ml-35 btn__confirm'>Confirm</a>
+		        </div>
+		    </div>
+		</div>
+	</div>
+</template>
+
+<script>
+    import formRowInput from './formRowInput.vue';
+	export default {
+	    components: { formRowInput},
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+		    selectTab(name) {
+		        this.tab = name;
+		    }
+		}
+	}
+</script>
+<style lang='scss' scoped>
+    .login__container {
+        position:absolute;
+        width:50vw;
+        height:auto;
+        top: calc(50vh - 25vh);
+        left: calc(50vw - 25vw);
+        border: 2px solid #ccc;
+    }
+    .login__tab {
+        height:50px;
+        width:100%;
+        display:flex;
+        li {
+            width:50%;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            color: steelblue;
+            border-bottom: 2px solid #ccc;
+            &.active {
+                 border-bottom: 2px solid steelblue;
+            }
+        }
+    }
+
+    .login__box {
+        padding-top:50px;
+        display:flex;
+        flex-direction: column;
+        justify-content:center;
+        align-items:center;
+
+        width:50%;
+        margin:auto;
+    }
+
+    .row {
+        width:100%;
+        height:40px;
+        margin-bottom:10px;
+    }
+    .btn__row {
+        display:inline-block;
+        width:100%;
+        height:30px;
+        line-height:30px;
+        text-align: center;
+        border-radius:5px;
+    }
+    .btn__blue {
+        background-color:steelblue;
+        color:white;
+    }
+    .form__code {
+        input {
+            width:40% !important;
+        }
+        a {
+            width:80px;
+            text-align:center;
+            height:30px;
+            line-height:30px;
+            background-color:grey;
+            display: inline-block;
+            font-size:15px;
+            vertical-align: top;
+        }
+    }
+    .ml-35 {
+        margin-left:35%;
+    }
+
+    .btn__confirm {
+         display:inline-block;
+         width:120px;
+         height:30px;
+         line-height:30px;
+         text-align: center;
+         border-radius:5px;
+         background-color:steelblue;
+         color:white;
+    }
+
+</style>
---
+++ vue-practise/form_page/demo01/src/formRowInput.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="form-row-input">
+        <label
+            :style="{
+                flex: '0 0 ' + w
+            }"
+        >
+            {{title}}
+        </label>
+        <div class='input__wrapper'>
+            <slot></slot>
+        </div>
+        <template v-if='title2'>
+            <label
+                class='form__title2'
+                :style="{
+                    flex: '0 0 ' + w
+                }"
+            >
+                {{title2}}
+            </label>
+            <div class='input__wrapper'>
+                <slot name="input2"></slot>
+            </div>
+        </template>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['w', 'title', 'title2'],
+    }
+</script>
+
+<style lang='scss' scoped>
+    .form-row-input {
+        width:100%;
+        height:30px;
+        display: flex;
+
+    }
+    label {
+        display:inline-block;
+        height:30px;
+        font-weight:600;
+        font-size:14px;
+        line-height:30px;
+        text-align:right;
+        padding-right:20px;
+
+    }
+    .form__title2 {
+         text-align:right;
+    }
+    .input__wrapper {
+        flex:1;
+        input {
+            height:30px;
+            line-height:30px;
+            padding-left:10px;
+            width:100%;
+        }
+    }
+
+</style>
