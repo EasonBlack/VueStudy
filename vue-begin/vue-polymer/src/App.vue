e473a5f31b9970bd6eab3ae3e52888c556fa59b7
EasonBlack <eason820720@163.com>
2017-09-03 23:20:38 +0800

    up

---
+++ vue-begin/vue-polymer/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <link rel="import" href="src/polymer-component/custom-element.html">
+        <div class='test' v-my-directive='message'  >
+        </div>
+        <div class='test' v-my-advance-directive='clickEvent'>
+             {{message}}
+        </div>
+        <custom-element></custom-element>
+    </div>
+
+</template>
+
+<script>
+    import myDirective from './my-directive';
+    import myAdvanceDirective from './my-advance-directive';
+	export default {
+        directives: { myDirective , myAdvanceDirective},
+		data() {
+			return {
+                message: 'hello'
+			}
+		},
+		methods: {
+            clickEvent() {
+                this.message = Math.random() * 10 + 1;
+            }
+		},
+		created:function() {
+
+		}
+
+	}
+</script>
+<style>
+    * {
+        padding:0;
+        margin:0;
+        box-sizing:border-box;
+        font-family: Arial;
+    }
+    .test {
+        width:100px;
+        height:100px;
+        background-color: steelblue;
+        margin-bottom:10px;
+    }
+</style>
