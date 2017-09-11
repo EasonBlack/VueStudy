44db98634cbbc34b24e338b64606057ab2d98c04
easonblack <eason820720@163.com>
2017-09-11 14:08:47 +0800

    up

---
+++ vue-chrome/basic/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<h1>
+		{{message}}
+	</h1>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				message: 'my vue'
+			}
+		}
+	}
+</script>
+<style>
+	h1 { font-size: 22px; }
+</style>
---
+++ vue-chrome/basic/webpack.config.js
@@ -0,0 +1,46 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'build.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.html$/,
+                loader: 'vue-html-loader'
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'url-loader',
+                query: {
+                    limit: 10000,
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue'
+        }
+    }
+}
+
