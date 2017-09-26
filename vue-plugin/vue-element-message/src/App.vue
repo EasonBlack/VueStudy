d4ef21100531a2eb3f2fecf78013cb600a27f040
easonblack <eason820720@163.com>
2017-09-26 15:52:19 +0800

    up

--- vue-plugin/vue-element-message/src/App.vue
+++ vue-plugin/vue-element-message/src/App.vue
@@ -1,12 +1,16 @@
 <template>
 	<div>
        <button @click="confirm">Confirm</button>
+       <button @click="success">Success</button>
+       <button @click="error">Error</button>
 	</div>
+    
 </template>
 
 <script>
     import { MessageBox } from 'element-ui'
 	export default {
+      
 		data() {
 			return {}
 		},
@@ -16,6 +20,24 @@
                     confirmButtonText: 'OK',
                     callback: action => {
                         console.log('yes');
+                    }
+                });
+            },
+            success() {
+                 MessageBox.alert('Save somthing successful ! ', 'Success', {
+                    confirmButtonText: 'OK',
+                    type: 'success',
+                    callback: action => {
+                        console.log('yes');
+                    }
+                });
+            },
+            error() {
+                MessageBox.alert('Somthing is wrong ! ', 'Error', {
+                    confirmButtonText: 'Return',
+                    type: 'error',
+                    callback: action => {
+                        console.log('error');
                     }
                 });
             }
