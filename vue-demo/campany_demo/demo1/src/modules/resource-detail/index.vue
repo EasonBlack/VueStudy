504462bd327c4ba41c5f666fb171c3dd3f029e9b
easonblack <eason820720@163.com>
2017-11-30 16:06:22 +0800

    up

--- vue-demo/campany_demo/demo1/src/modules/resource-detail/index.vue
+++ vue-demo/campany_demo/demo1/src/modules/resource-detail/index.vue
@@ -3,14 +3,14 @@
         <div class='search'>
             <div class='search-box'>
                 <label>Location</label>
-                <select>
-                    <option>aaaaa</option>
+                <select v-model='selectLocation' @change='locationChange'>
+                    <option v-for='(location, $index) in locations' :value='location' :key='$index'>{{location.name}}</option>
                 </select>
             </div>
             <div class='search-box'>
                 <label>Plant</label>
-                <select>
-                    <option>aaaaa</option>
+                <select v-model='selectPlant' @change='plantChange'>
+                    <option  v-for='(plant, $index) in selectLocation.plants' :value='plant'  :key='$index'>{{plant.name}}</option>
                 </select>
             </div>
             <a>Show Detail</a>
@@ -21,8 +21,8 @@
                 <li>BESS</li>
             </ul>
             <div class='content-type'>
-                <select>
-                    <option>bbbbb</option>
+                <select v-model='selectUnit' @change='unitChange'>
+                    <option v-for='(unit, $index) in selectPlant.units' :value='unit'  :key='$index'>{{unit.name}}</option>
                 </select>
             </div>
             <div class='content__wrapper'>
@@ -63,9 +63,73 @@
     export default  {
         data() {
             return {
-                items: [
-
-                ]
+                
+                // selectPlant: 'Plant 1',
+                locations: [
+                    {name: "Location 1", plants: [
+                        {name: "Plant 1", units: [
+                            {name: 'P1-Solar1' , type: 'solor'},
+                            {name: 'P1-Solar2' , type: 'solor'},
+                            {name: 'P1-Battery1' , type: 'battery'},
+                            {name: 'P1-Battery2' , type: 'battery'},
+                            {name: 'P1-Wind1' , type: 'wind'},
+                            {name: 'P1-Wind2' , type: 'wind'},
+                            {name: 'P1-Gen1' , type: 'gen'},
+                            {name: 'P1-Gen2' , type: 'gen'},
+                        ]},
+                        {name: "Plant 2", units: [
+                            {name: 'P2-Solar1' , type: 'solor'},
+                            {name: 'P2-Solar2' , type: 'solor'},
+                            {name: 'P2-Battery1' , type: 'battery'},
+                            {name: 'P2-Battery2' , type: 'battery'},
+                            {name: 'P2-Wind1' , type: 'wind'},
+                            {name: 'P2-Wind2' , type: 'wind'},
+                            {name: 'P2-Gen1' , type: 'gen'},
+                            {name: 'P2-Gen2' , type: 'gen'},
+                        ]},
+                    ]},
+                    {name: "Location 2", plants: [
+                        {name: "Plant 3", units: [
+                            {name: 'P3-Solar1' , type: 'solor'},
+                            {name: 'P3-Solar2' , type: 'solor'},
+                            {name: 'P3-Battery1' , type: 'battery'},
+                            {name: 'P3-Battery2' , type: 'battery'},
+                            {name: 'P3-Wind1' , type: 'wind'},
+                            {name: 'P3-Wind2' , type: 'wind'},
+                            {name: 'P3-Gen1' , type: 'gen'},
+                            {name: 'P3-Gen2' , type: 'gen'},
+                        ]},
+                        {name: "Plant 4", units: [
+                            {name: 'P4-Solar1' , type: 'solor'},
+                            {name: 'P4-Solar2' , type: 'solor'},
+                            {name: 'P4-Battery1' , type: 'battery'},
+                            {name: 'P4-Battery2' , type: 'battery'},
+                            {name: 'P4-Wind1' , type: 'wind'},
+                            {name: 'P4-Wind2' , type: 'wind'},
+                            {name: 'P4-Gen1' , type: 'gen'},
+                            {name: 'P4-Gen2' , type: 'gen'},
+                        ]},
+                    ]}
+                ],
+                selectLocation: null,
+                selectPlant: null,
+                selectUnit: null
+                
+            }
+        },
+        created() {
+            this.selectLocation = this.locations[0];
+            this.selectPlant = this.selectLocation.plants[0]
+        },
+        methods: {
+            locationChange() {
+               this.selectPlant = this.selectLocation.plants[0]
+            },
+            plantChange() {
+               //this.selectPlant = this.selectLocation.plants[0]
+            },
+            unitChange() {
+                
             }
         }
     }
