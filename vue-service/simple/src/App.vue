962dfb664404a63b80588c802396d95e5fdc283f
easonblack <eason820720@163.com>
2017-12-13 09:38:08 +0800

    up

--- vue-service/simple/src/App.vue
+++ vue-service/simple/src/App.vue
@@ -7,8 +7,8 @@
 </template>
 
 <script>
-	import com1 from './component/com1.vue'
-	import com2 from './component/com2.vue'
+	import com1 from './component/com_one.vue'
+	import com2 from './component/com_two.vue'
 	import displayDiv from './component/display.vue'
 	export default {
 		components:{com1, com2, displayDiv},
