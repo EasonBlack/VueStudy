b8fd891ec48b8b8ff31a6570a494c169f2199ff5
EasonBlack <eason820720@163.com>
2018-03-17 22:30:29 +0800

    up

---
+++ vue-bootstrap/form-practise/demo1/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class='container-fluid' style='padding-top:50px;'>
+		<form>
+			<div class='row'>
+				<div class='col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4'>
+					<input-group :title='"First Name"' />
+				</div>
+				<div class='col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4'>
+					<input-group :title='"Middle Name"' />
+				</div>
+				<div class='col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4'>
+					<input-group :title='"Last Name"' />	
+				</div>
+			
+				<div class='col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4'>
+					<input-group :title='"User ID"' />
+				</div>
+				<div class='col-xs-12 col-sm-12 col-md-8 col-lg-8 col-xl-8'>
+					<input-group :title='"Email"' />
+				</div>
+		
+				<div class='col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4'>
+					<input-group :title='"Mobile"' />
+				</div>
+				<div class='col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-4'>
+					<input-group :title='"Office"' />
+				</div>
+				
+			</div>
+		</form>
+	</div>
+</template>
+
+<script>
+	import inputGroup from './group.vue'
+	export default {
+		components: {inputGroup},
+		data() {
+			return {
+				message: 'my vue'
+			}
+		}
+	}
+</script>
+<style>
+
+</style>
---
+++ vue-bootstrap/form-practise/demo1/src/group.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class='form-group d-flex w-100'>
+        <label>{{title}}</label>
+        <input class='form-control' />
+    </div>
+</template>
+<script>
+    export default {
+        props: ["title"]
+    }
+</script>
+<style lang='scss' scoped>
+
+    label {
+        width:120px;
+        min-width:120px;
+        line-height: 15px;
+        display: flex;
+        align-items: center;
+    }
+   
+</style>
