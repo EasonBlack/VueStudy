c12d4ff8f97c1af532b28c3b822536c4d92847fe
easonblack <eason820720@163.com>
2018-04-12 13:57:35 +0800

    up

---
+++ vue-practise/treeshake-by-python/project-src/App.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+	div
+		include App.html
+		include App1.html
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				message1: 'This is project A',
+				message2: 'This is project B'
+			}
+		}
+	}
+</script>
+<style>
+
+</style>
