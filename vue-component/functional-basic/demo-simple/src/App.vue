dd55b7347874e866ab13e7fb12413a5e48ff7a47
easonblack <eason820720@163.com>
2018-06-04 11:01:31 +0800

    up

---
+++ vue-component/functional-basic/demo-simple/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<div>{{msg}}</div>
+		<my-com :message='"aaaaa"' />
+	</div>
+</template>
+
+<script>
+	import myCom from './component.vue';
+	export default {
+		components: {myCom},
+		data() {
+			return {
+				msg: 'my vue'
+			}
+		}
+	}
+</script>
+<style>
+
+</style>
---
+++ vue-component/functional-basic/demo-simple/src/component.vue
@@ -0,0 +1,11 @@
+<template functional>
+    <div>Hello world 222 {{props.message}}</div>
+</template>
+
+<script>
+    export default {
+        props: ['message']
+    }
+</script>
+
+
