e358196dc6179d4d0a11cf1448df34da7cefbf19
easonblack <eason820720@163.com>
2018-08-20 16:35:11 +0800

    up

--- vue-table/table-nested/src/App.vue
+++ vue-table/table-nested/src/App.vue
@@ -7,8 +7,15 @@
 				<th>CONTENT</th>
 			</thead>
 			<tbody>
-				<trwrapper :items='tableData' />
-			</tbody>
+				<tr v-for='item in tableData' :key='item.ID'  @click='toggleChildren(item)'
+					:style='{"backgroundColor": item.color}'
+				>
+					<td>{{item.id}}</td>
+					<td>{{item.name}}</td>
+					<td>{{item.content}}</td>
+				</tr>
+				
+			</tbody>	
 		</table>
 	</div>
 </template>
@@ -21,7 +28,9 @@
 		data() {
 			return {
 				message: 'my vue',
-				tableData: []
+				tableData: [],
+				middle :{},
+				colors: ['red', 'lightgreen', 'steelblue', 'pink', 'yellow']
 			}
 		},
 		created() {
@@ -33,8 +42,11 @@
 				}
 				middle[r.parent].push(r);
 			}		
-			this.handle(middle['0'], middle);
+			this.middle = middle;
+			//this.handle(middle['0'], middle);
+			
 			this.tableData = [...middle['0']];
+			console.log(this.tableData);
 		},
 		methods: {
 			handle(items, source) {
@@ -45,17 +57,58 @@
 						this.handle(items[i].children, source);
 					}
 				}
+			},
+
+			toggleChildren(item) {
+				let idx = this.tableData.findIndex(t=>item.id == t.id);
+				console.log(this.tableData[idx]);
+				if(!this.tableData[idx].expand) {
+					this.tableData[idx].expand = true;
+				} else {
+					this.tableData[idx].expand = false;
+					this.deleteChidren(item);
+					return false;
+				}
+			
+				let all = [...this.tableData];
+				let _first = all.splice(0, idx + 1);
+				let insert = this.middle[item.id] || [];
+				insert = [...insert]
+				if(insert.length) {
+					let _cidx = (item.colorIndex||0) + 1;
+					insert.map(n=>Object.assign(n, {color: this.colors[_cidx], colorIndex: _cidx}));
+				}
+				this.tableData = [..._first, ...insert, ...all];
+				console.log(this.tableData);		
+			},
+
+			deleteChidren(item) {
+				let _items = [...this.tableData];
+				let idx = this.tableData.findIndex(t=>item.id == t.id);
+				let idx2;
+				let _parents =[];
+				for(let i in _items) {
+					if(i>idx) {
+						if(_items[i].parent == item.parent) {
+							idx2 = i;
+							break;
+						}
+					}
+				}
+				
+				if(idx2) {
+					console.log(idx, idx2);
+					_items.splice(idx+1, idx2-(idx+1));
+				} else {
+					_items.splice(idx+1, 999);
+				}
+				this.tableData =  [..._items];
+					
 			}
 		}
 	}
 </script>
 <style lang='scss'>
-	.container {
-
-	}
-
-	table p {
 	
-	}
 
 </style>
